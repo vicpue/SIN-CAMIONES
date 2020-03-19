1199dd5b2262165b357b4ac53d0601804b6b9f76
vicpue <vicentcastell21@gmail.com>
2020-03-19 20:03:45 +0100

    initial commit

---
+++ ejercicio1.py
@@ -0,0 +1,26 @@
+import pyhop
+
+#ESTADO INICIAL
+
+state1=pyhop.State('state1')
+state1.ciudad=['C0', 'C1', 'C2']
+state1.parada=['S1', 'S2']
+state1.camion=['T1', 'T2']
+state1.conductor=['D1', 'D2']
+state1.paquete=['P1', 'P2']
+state1.senda={  'W1':['C0','S1'], 
+                'W2':['S1', 'C1'],
+                'W3':['C1','S2'],
+                'W4':['S2','C2']
+                }
+state1.carretera={  'H01';['C0','C1'],
+                    'H12':['C1','C2'],
+                    'H02':['C0','C2']
+                    }
+
+#ESTADO DINAMICO
+
+state1.ubi_camion ={'T1':'C1','T2':'C0'}
+state1.ubi_conductor={'D1':'S1','D2':'C1'}
+state1.ubi_paquete={'P1':'C0','P2':'C0'}
+
